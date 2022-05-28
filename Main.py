a143bcf57083607605e00deeb6a6badd59b759bb
BruceKing <49490329+JSYBruce@users.noreply.github.com>
2022-05-28 17:29:26 +0800

    Add files via upload

--- Main.py
+++ Main.py
@@ -13,8 +13,7 @@ import numpy as np
 import pandas as pd
 
 def runModel(data_file_name):
-  
-    df = pd.read_excel("AR_data.xlsx", header=None, names = ["bas"])
+    df = pd.read_excel(data_file_name, header=None, names = ["bas"])
     archmodel = ARCHModel(y = df.bas, distribution = LogNormal(), volatility = GARCH())
     result = archmodel.fit(starting_values=[2, 0.23763582, 0.2    ,0.2])
     result = ARCHModelResult(result[0], None,  result[1],  result[2], result[3], result[4], result[5], result[6], result[7], result[8], result[9], result[10], result[11], result[12])
